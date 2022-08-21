7cc705a1f08ebcc9c0f330bd42bab20ae0201b34
fabiojrfla <fabiojrfla@gmail.com>
2022-08-21 11:11:40 -0300

    test: add bank account withdraw test

---
+++ spec/system/withdraws/client_withdraws_from_his_account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Client withdraws from his bank account' do
+  it 'successfully' do
+    client = create(:client)
+    create(:credit, amount: 100_000, bank_account: client.bank_account)
+
+    login_as client
+    visit root_path
+    click_on 'Sacar'
+    fill_in 'Valor R$', with: '1000'
+    click_on 'Sacar'
+
+    expect(current_path).to eq new_deposit_path
+    expect(page).to have_content 'Saque realizado com sucesso.'
+    expect(page).to have_content 'Seu saldo: R$ 0,00'
+  end
+end
