b9f34885d656456483414fb02f44470a17b74354
fabiojrfla <fabiojrfla@gmail.com>
2022-08-21 09:38:32 -0300

    test: adds bank account balance check test

---
+++ spec/system/bank_accounts/client_checks_his_account_balance_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Client checks his bank account balance' do
+  it 'successfully' do
+    client = create(:client)
+    create(:credit, amount: 100_000, bank_account: client.bank_account)
+
+    login_as client
+    visit root_path
+    click_on 'Saldo'
+
+    expect(current_path).to eq balance_bank_accounts_path
+    within 'h2' do
+      expect(page).to have_content 'Saldo'
+    end
+    expect(page).to have_content 'R$ 1.000,00'
+    expect(page).to have_link 'Voltar'
+  end
+end
